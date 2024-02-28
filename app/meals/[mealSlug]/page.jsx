a4c765404317bd3ed5c50577f77783b4c2dcde76
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-02-28 17:47:44 +0100

    Meal page

--- app/meals/[mealSlug]/page.jsx
+++ app/meals/[mealSlug]/page.jsx
@@ -1,5 +1,36 @@
+import style from './page.module.css'
+import Image from "next/image";
+import {getMeal} from "@/lib/meals";
+
 export default function Meal({params}) {
+    const meal = getMeal(params.mealSlug);
+
+    meal.instructions = meal.instructions.replace(/\n/g, '<br />')
+
     return (
-        <h1>Meal {params.mealSlug}</h1>
+        <>
+            <header className={style.header}>
+                <div className={style.image}>
+                    <Image fill
+                           src={meal.image}
+                           alt={meal.title}/>
+                </div>
+                <div className={style.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={style.creator}>
+                        By <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+                    </p>
+                    <p className={style.summary}>
+                        {meal.summary}
+                    </p>
+                </div>
+            </header>
+            <main>
+                <p className={style.instructions}
+                   dangerouslySetInnerHTML={{
+                       __html: meal.instructions,
+                   }}></p>
+            </main>
+        </>
     )
 }
